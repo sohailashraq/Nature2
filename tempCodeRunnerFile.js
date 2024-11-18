5c2262a355f8cd775cfaab3fe5875378dd31d26a
sohailashraq <sohailashraq2002@gmail.com>
2024-11-18 04:36:18 -1100

    Add files via upload

---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,15 @@
+const num1 = 76324;
+const num2 = 71324;
+
+if(num1 > num2){
+  console.log("num1 is the greatest")
+}
+else if (num2 >num1){
+  console.log("num2 is the greatest")
+}
+else if (num1 === num2) {
+  console.log("both are equal")
+}
+else {
+  console.log("Not equal")
+}
